7f3f3c7d1ff4121535cce51263e8c8de9196d388
“ongks”-useR” <ongks@Ongs-MacBook-Pro.local>
2022-05-18 20:58:21 +0800

    version 1.0 of calculator after multiple trial and errors.

--- control.js
+++ control.js
@@ -1,32 +1,145 @@
-// const result = document.querySelector('#display');
 const numbers = document.querySelectorAll('.number');
-const icons = document.querySelectorAll('img');
+const percent = document.querySelector('#percent');
+const reset = document.querySelector('#reset');
+const backspace = document.querySelector('#delete');
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('#equal');
 
-let total = '';
+let final = '';
+let answer = '';
+let temp = '';
 
-function displayNumber(e) {
+numbers.forEach(number => number.addEventListener('click', e => {
     const display = document.querySelector('#display');
-    const number = e.target.getAttribute('id');
+    final += e.target.textContent;
+    display.textContent = final;
+}))
 
-    total += number;
-    display.textContent = total;
-}
-
-function operators(e) {
+percent.addEventListener('click', e => {
     const display = document.querySelector('#display');
-    let result = display.textContent;
+    final += '%';
+    display.textContent = final;
+})
 
-    const action = e.target.getAttribute('id');
+reset.addEventListener('click', e => {
+    const display = document.querySelector('#display');
+    const t = document.querySelector('#answer');
 
-    console.log(result);
+    final = '';
+    answer = '';
+    temp = '';
 
-    switch (action) {
-        case 'del':
-            total = result.slice(0, -1);
-            display.textContent = total;
-            console.log(total);
+    display.textContent = final;
+    t.textContent = temp;
+})
+
+backspace.addEventListener('click', e => {
+    const display = document.querySelector('#display');
+    final = display.textContent.slice(0, -1);
+    display.textContent = final;
+})
+
+operators.forEach(operator => operator.addEventListener('click', e => {
+    const display = document.querySelector('#display');
+    const t = e.target.getAttribute('id');
+
+    switch (t) {
+        case ('divide'):
+            final += '/';
+            display.textContent = final;
+            break;
+
+        case ('multiply'):
+            final += 'x';
+            display.textContent = final;
+            break;
+
+        case ('subtract'):
+            final += '-';
+            display.textContent = final;
+            break;
+
+        case ('sum'):
+            final += '+';
+            display.textContent = final;
+            break;
     }
-}
+}))
 
-numbers.forEach(num => num.addEventListener('click', displayNumber))
-icons.forEach(icon => icon.addEventListener('click', operators));
+equal.addEventListener('click', e => {
+    const display = document.querySelector('#display');
+    const t = document.querySelector('#answer');
+
+    if (display.textContent.length !== 0) {
+
+        answer = display.textContent.replaceAll('+', '|+|')
+            .replaceAll('-', '|-|')
+            .replaceAll('x', '|x|')
+            .replaceAll('/', '|/|')
+            .split('|')
+
+        answer = answer.map((e, i) => {
+            if (i % 2 === 0) {
+                if (e.endsWith('%')) {
+                    e = +e.slice(0, 2) / 100.0;
+                    return e;
+                }
+                else if (e === '') {
+                    return;
+                }
+                else if (typeof +e === 'number') {
+                    return +e;
+                }
+            }
+            else {
+                return e;
+            }
+        })
+
+        let op = answer.filter(e => typeof e === 'string')
+
+        if (op.includes('+') || op.includes('-') || op.includes('x') || op.includes('/')) {
+            op.forEach(e => {
+
+                switch (e) {
+                    case '+':
+                        temp = answer[0] + answer[2];
+                        answer.splice(0, 3, temp);
+                        break;
+
+                    case '-':
+                        temp = answer[0] - answer[2];
+                        answer.splice(0, 3, temp);
+                        break;
+
+                    case 'x':
+                        temp = answer[0] * answer[2];
+                        answer.splice(0, 3, temp);
+                        break;
+
+                    case '/':
+                        if (answer[2] !== 0.0) {
+                            temp = answer[0] / answer[2];
+                            answer.splice(0, 3, temp);
+                        }
+                        break;
+                }
+            })
+        }
+        else {
+            temp = answer[0];
+        }
+
+        if (!isNaN(temp)) {
+            if (Number.isInteger(temp)) {
+                t.textContent = temp
+            }
+            else {
+                t.textContent = temp.toFixed(4);
+            }
+        }
+        else { return; }
+
+    }
+    else { return; }
+})
